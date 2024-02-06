9f324381982d786ea427f5daac2176384206cf20
Shubham <shubham.shubham.singh13@gmail.com>
2024-02-06 19:05:52 +0530

    dependency injection using @Component and Constructor injection

---
+++ 02-Spring-boot-Core/01-Construction-Injection/src/main/java/com/learningSpring/leaningDependencyInjection/Coach.java
@@ -0,0 +1,5 @@
+package com.learningSpring.leaningDependencyInjection;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-Spring-boot-Core/01-Construction-Injection/src/main/java/com/learningSpring/leaningDependencyInjection/LeaningDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.learningSpring.leaningDependencyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaningDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaningDependencyInjectionApplication.class, args);
+	}
+
+}
